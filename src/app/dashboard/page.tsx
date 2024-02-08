60bfc12dddac5952bbdf2b27f73d580c9ede831e
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-02-08 08:20:55 +0530

    initial commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import Button from '@/components/ui/Button'
+import React, { FC } from 'react'
+
+interface pageProps {}
+
+const page:FC<pageProps> = ({}) => {
+  return (
+    <Button>
+      Hello
+    </Button>
+  )
+}
+
+export default page
