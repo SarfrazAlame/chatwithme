a9792054f9f040e6f7236240f28fa9f37b90ed03
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-02-10 10:31:14 +0530

    schema creation

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -3,6 +3,8 @@ import React, { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Input from "./ui/Input";
 import Button from "./ui/Button";
+import AuthSocialButton from "./ui/AuthSocialButton";
+import { BsGithub, BsGoogle } from "react-icons/bs";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -51,12 +53,69 @@ const AuthForm = () => {
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          <Input id="email" label="Email" register={register} errors={errors} />
-          <Input id="password" label="Password" register={register} errors={errors} />
+          {variant === "REGISTER" && (
+            <Input
+              id="Name"
+              label="Name"
+              register={register}
+              errors={errors}
+              disabled={isLoading}
+            />
+          )}
+          <Input
+            id="email"
+            label="Email Address"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Input
+            id="password"
+            label="Password"
+            register={register}
+            disabled={isLoading}
+            errors={errors}
+          />
+
           <div>
-            <Button></Button>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Sign in" : "Register"}
+            </Button>
           </div>
         </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue With
+              </span>
+            </div>
+          </div>
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+          </div>
+        </div>
+
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          <div>
+            {variant === "LOGIN"
+              ? "New to Messanger?"
+              : "Already have an account?"}
+          </div>
+          <div onClick={toggleVariant} className="underline cursor-pointer">
+            {variant === "LOGIN" ? "Create an account" : "Login"}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -9,6 +9,7 @@ interface ButtonProsp {
   children?: React.ReactNode;
   secondary?: boolean;
   danger?: boolean;
+  onClick?: () => void;
   disabled?: boolean;
 }
 
@@ -17,10 +18,32 @@ const Button: React.FC<ButtonProsp> = ({
   fullWidth,
   children,
   secondary,
+  onClick,
   danger,
   disabled,
 }) => {
-  return <div></div>;
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+      className={clsx(
+        `
+    flex justify-center rounded-md px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
+    `,
+        disabled && "opacity-50 cursor-default",
+        fullWidth && "w-full",
+        secondary ? "text-gray-900" : "text-white",
+        danger &&
+          "bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600",
+        !secondary &&
+          !danger &&
+          "bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600"
+      )}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
